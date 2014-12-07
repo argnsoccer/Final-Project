74c5fcbaea4f8cca674efc043396ee55e911bceb
argnsoccer <amgras@smu.edu>
2014-12-06 18:14:10 -0600

    pbbblltt.

--- FinalProject/driver.cpp
+++ FinalProject/driver.cpp
@@ -4,6 +4,8 @@ Driver::Driver(char *fileName)
 {
     int inputCommand;
     i = new AVLTree(p.parse(fileName));
+    cout << "Index has been parsed" << endl;
+    cout << "github modifications.";
     i->save();
     cout << "Which mode would you like to be in? (Please only integer answers)" << endl;
     cout << "1: Maintenance Mode \n2: Interactive Mode " << endl;
