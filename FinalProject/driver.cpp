9b654ae9f722ccf99b110d42d2d964300bb126dd
argnsoccer <amgras@smu.edu>
2014-12-06 21:31:47 -0600

    pushing driver changes

--- FinalProject/driver.cpp
+++ FinalProject/driver.cpp
@@ -3,43 +3,86 @@
 Driver::Driver(char *fileName)
 {
     int inputCommand;
-    i = new AVLTree(p.parse(fileName));
-    cout << "Index has been parsed" << endl;
-    i->save();
-    cout << "Which mode would you like to be in? (Please only integer answers)" << endl;
-    cout << "1: Maintenance Mode \n2: Interactive Mode " << endl;
-    cin >> inputCommand;
-    switch(inputCommand)
+    bool saver = false;
+    while(saver == false)
     {
+        cout << "Parse and save a new file or load from file?" << endl;
+        cout << "1: Save\n2: Load" << endl;
+        cin >> inputCommand;
+        if(inputCommand == 1)
+        {
+            i = new AVLTree(p.parse(fileName));
+            cout << "Index has been parsed" << endl;
+            i->save();
+            saver = true;
+        }
+        else if(inputCommand == 2)
+        {
+            i = new AVLTree();
+            cout << "You will load from the file." << endl;
+            saver = true;
+        }
+        else
+        {
+            saver = false;
+        }
+    }
+    bool cleared = false;
+    bool looper=false;
+    while(looper == false)
+    {
+        cout << "Which mode would you like to be in? (Please only integer answers)" << endl;
+        cout << "1: Maintenance Mode \n2: Interactive Mode\nAny other number to quit" << endl;
+        cin >> inputCommand;
+
+        switch(inputCommand)
+        {
         //Maintenance Mode
         case 1:
-            cout << "What would you like to do in Maintenance Mode?" << endl;
-            cout << "1: Clear Index \n2: Append a file" << endl;
-            cin >> inputCommand;
-            switch(inputCommand)
+        {
+            bool maintLoop = false;
+            while(maintLoop == false)
             {
-            case 1:
-                i->clearIndex();
-                break;
+                cout << "What would you like to do in Maintenance Mode?" << endl;
+                cout << "1: Clear Index \n2: Append a file\nAny other number to quit" << endl;
 
-            case 2:
-                //i->appendFile();
-                break;
+                cin >> inputCommand;
+                switch(inputCommand)
+                {
+                case 1:
+                    i->clearIndex();
+                    cleared = true;
+                    break;
 
+                case 2:
+                    //i->appendFile();
+                    break;
+
+                default:
+                    maintLoop = true;
+                    break;
+
+                }
             }
 
-            //i->clearIndex();
-            //i->appendFile();
             break;
+        }
 
         //Interactive Mode
         case 2:
+        {
             cout << "Index loaded into AVLTree for you. Sorry for the lack of variety...and speed." << endl;
             i->load();
-
-
             break;
+        }
 
+        default:
+        {
+            looper = true;
+            break;
+        }
+
+        }
     }
 
 }
